821ef22188c39b949353ac81997349844924fe95
SamGraber <samgraber@sundialproperties.com>
2016-04-25 12:12:34 -0400

    Rx custom typings

--- typings/custom.d.ts
+++ typings/custom.d.ts
@@ -1,1 +1,16 @@
 declare var require: Function;
+
+declare module "rx" {
+	export = Rx;
+}
+
+declare module Rx {
+	class Subject<T> {
+		onNext(value: T): void;
+		subscribe(onNext: { (value: T): void }, onError?: { (error: any): void }, onCompleted?: {(): void}): Subscriber;
+	}
+
+	interface Subscriber {
+		dispose(): void;
+	}
+}
