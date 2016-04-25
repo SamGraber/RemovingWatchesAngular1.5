11ba5759ce4b8330f25907b0094e5decdc2a09d6
SamGraber <samgraber@sundialproperties.com>
2016-04-25 12:05:32 -0400

    Build

--- source/watchInTemplate/watchInTemplate.js
+++ source/watchInTemplate/watchInTemplate.js
@@ -2,13 +2,19 @@
 var angular = require('angular');
 exports.moduleName = 'watchInTemplate';
 var WatchInTemplateController = (function () {
-    function WatchInTemplateController($scope) {
-        var _this = this;
-        $scope.$watch('controller.watchedValue', function (value) {
-            _this.doubledValue = _this.watchedValue * 2;
-        });
+    function WatchInTemplateController() {
     }
-    WatchInTemplateController.$inject = ['$scope'];
+    Object.defineProperty(WatchInTemplateController.prototype, "watchedValue", {
+        get: function () {
+            return this._watchedValue;
+        },
+        set: function (value) {
+            this._watchedValue = value;
+            this.doubledValue = this._watchedValue * 2;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return WatchInTemplateController;
 }());
 function watchInTemplate() {
@@ -23,4 +29,4 @@ function watchInTemplate() {
 angular.module(exports.moduleName, [])
     .controller('WatchInTemplateController', WatchInTemplateController)
     .directive('tsWatchInTemplate', watchInTemplate);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2F0Y2hJblRlbXBsYXRlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsid2F0Y2hJblRlbXBsYXRlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxJQUFZLE9BQU8sV0FBTSxTQUFTLENBQUMsQ0FBQTtBQUV0QixrQkFBVSxHQUFXLGlCQUFpQixDQUFDO0FBRXBEO0lBS0MsbUNBQVksTUFBc0I7UUFMbkMsaUJBVUM7UUFKQyxNQUFNLENBQUMsTUFBTSxDQUFDLHlCQUF5QixFQUFFLFVBQUMsS0FBYTtZQUN0RCxLQUFJLENBQUMsWUFBWSxHQUFHLEtBQUksQ0FBQyxZQUFZLEdBQUcsQ0FBQyxDQUFDO1FBQzNDLENBQUMsQ0FBQyxDQUFDO0lBQ0osQ0FBQztJQUxNLGlDQUFPLEdBQWEsQ0FBQyxRQUFRLENBQUMsQ0FBQztJQU12QyxnQ0FBQztBQUFELENBQUMsQUFWRCxJQVVDO0FBRUQ7SUFDQyxNQUFNLENBQUM7UUFDTixRQUFRLEVBQUUsR0FBRztRQUNiLFFBQVEsRUFBRSxPQUFPLENBQUMsd0JBQXdCLENBQUM7UUFDM0MsVUFBVSxFQUFFLDJCQUEyQjtRQUN2QyxZQUFZLEVBQUUsWUFBWTtRQUMxQixLQUFLLEVBQUUsRUFBRTtLQUNULENBQUM7QUFDSCxDQUFDO0FBRUQsT0FBTyxDQUFDLE1BQU0sQ0FBQyxrQkFBVSxFQUFFLEVBQUUsQ0FBQztLQUM1QixVQUFVLENBQUMsMkJBQTJCLEVBQUUseUJBQXlCLENBQUM7S0FDbEUsU0FBUyxDQUFDLG1CQUFtQixFQUFFLGVBQWUsQ0FBQyxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2F0Y2hJblRlbXBsYXRlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsid2F0Y2hJblRlbXBsYXRlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxJQUFZLE9BQU8sV0FBTSxTQUFTLENBQUMsQ0FBQTtBQUV0QixrQkFBVSxHQUFXLGlCQUFpQixDQUFDO0FBRXBEO0lBQUE7SUFZQSxDQUFDO0lBUkEsc0JBQUksbURBQVk7YUFBaEI7WUFDQyxNQUFNLENBQUMsSUFBSSxDQUFDLGFBQWEsQ0FBQztRQUMzQixDQUFDO2FBRUQsVUFBaUIsS0FBYTtZQUM3QixJQUFJLENBQUMsYUFBYSxHQUFHLEtBQUssQ0FBQztZQUMzQixJQUFJLENBQUMsWUFBWSxHQUFHLElBQUksQ0FBQyxhQUFhLEdBQUcsQ0FBQyxDQUFDO1FBQzVDLENBQUM7OztPQUxBO0lBTUYsZ0NBQUM7QUFBRCxDQUFDLEFBWkQsSUFZQztBQUVEO0lBQ0MsTUFBTSxDQUFDO1FBQ04sUUFBUSxFQUFFLEdBQUc7UUFDYixRQUFRLEVBQUUsT0FBTyxDQUFDLHdCQUF3QixDQUFDO1FBQzNDLFVBQVUsRUFBRSwyQkFBMkI7UUFDdkMsWUFBWSxFQUFFLFlBQVk7UUFDMUIsS0FBSyxFQUFFLEVBQUU7S0FDVCxDQUFDO0FBQ0gsQ0FBQztBQUVELE9BQU8sQ0FBQyxNQUFNLENBQUMsa0JBQVUsRUFBRSxFQUFFLENBQUM7S0FDNUIsVUFBVSxDQUFDLDJCQUEyQixFQUFFLHlCQUF5QixDQUFDO0tBQ2xFLFNBQVMsQ0FBQyxtQkFBbUIsRUFBRSxlQUFlLENBQUMsQ0FBQyJ9
